a7b5677df81a9248755347d0c9023bd4adf078c7
TDYipengTan <yipeng.tan@china.talkdesk.com>
2022-05-12 16:47:27 +0800

    feat: support less, colbat component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import './App.css';
-
+import Flex from '@cobalt/react-flex';
 import React from 'react';
 
+import styles from './App.module.less';
+
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <Flex direction="column" width="100%">
+      <Flex className={styles.header}>Hello</Flex>
+      <Flex className={styles.header}>World</Flex>
+      <Flex className={styles.header}>!</Flex>
+    </Flex>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,17 @@
+import ThemeProvider from '@cobalt/react-theme-provider';
+import ViewportProvider from '@cobalt/react-viewport-provider';
+import Theme from '@cobalt/theme-experimental';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import styles from './variable-coverage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider style={styles} loader={() => Promise.resolve(Theme)}>
+    <ViewportProvider>
+      <App />
+    </ViewportProvider>
+  </ThemeProvider>,
+  document.getElementById('root'),
+);
