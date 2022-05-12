56aa7f614bc3db4e156022b890432dedbeea9646
TDYipengTan <yipeng.tan@china.talkdesk.com>
2022-05-12 11:13:23 +0800

    feat: add husky & delete unused code

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
-import './index.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root'),
 );
