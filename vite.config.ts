e0f113494f5866111bc153f808b4cdcfd06c7aa8
TDYipengTan <yipeng.tan@china.talkdesk.com>
2022-05-12 18:58:28 +0800

    feat: support colbat component

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
-import { defineConfig } from 'vite';
+import path from 'path';
+import { defineConfig, UserConfigFn } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const userConfigFn: UserConfigFn = (env) => {
+  return {
+    base:
+      env.mode === 'development' ? '' : 'https://tdyipengtan.github.io/react-storyline/',
+    resolve: {
+      alias: {
+        '@cobalt': path.resolve('./cobalt_modules/node_modules/@cobalt'),
+      },
+    },
+    plugins: [reactRefresh(), tsconfigPaths()],
+  };
+};
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: 'https://tdyipengtan.github.io/react-storyline/',
-  plugins: [reactRefresh(), tsconfigPaths()],
-});
+export default defineConfig(userConfigFn);
