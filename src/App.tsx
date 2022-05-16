069bc041ab55b003b582b9d473ba5a5482bfab90
TDYipengTan <yipeng.tan@china.talkdesk.com>
2022-05-16 18:06:25 +0800

    feat: test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import styles from './App.module.less';
 
-console.log(import.meta.env);
+console.log('>>>', import.meta.env);
 
 function App() {
   return <div className={`${styles.header} header`}>hello world!!!</div>;
