443d9498c937ee9c709ecee769c9b2e0a230c9a8
TDYipengTan <yipeng.tan@china.talkdesk.com>
2022-05-16 17:31:36 +0800

    feat: support env

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import styles from './App.module.less';
 
+console.log(import.meta.env);
+
 function App() {
   return <div className={`${styles.header} header`}>hello world!!!</div>;
 }
